265e2fbf32f61309c66d0bbedb15424c9875a67b
inco <269361@gmail.com>
2015-12-14 13:41:40 -0800

    utworzenie messages

---
+++ Messages/MessageGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Messages
+{
+    /// <summary>
+    /// klasa odpowiedzialna za generowanie wiadomosci i ich dekodowanie
+    /// bez konstruktora, zbior metod
+    /// </summary>
+    class MessageGenerator
+    {
+        public Messages dekoduj(byte[] data)
+        {
+            string xmlmessage;
+
+            XmlDocument doc = new XmlDocument();
+            xmlmessage = Encoding.UTF8.GetString(data);
+            xmlmessage = xmlmessage.Trim();
+            doc.LoadXml(xmlmessage);
+
+            Messages mes = new Messages(doc.DocumentElement.SelectSingleNode("message/id").InnerText);
+            mes.body = doc.DocumentElement.SelectSingleNode("message/body").InnerText;
+            mes.to = doc.DocumentElement.SelectSingleNode("message/to").InnerText;
+            mes.from = doc.DocumentElement.SelectSingleNode("message/from").InnerText;
+            mes.body = doc.DocumentElement.SelectSingleNode("message/body").InnerText;
+            mes.stringToKomenda(doc.DocumentElement.SelectSingleNode("message/body").InnerText);
+            return mes;
+        }
+
+        public byte[] koduj(Messages mes)
+        {
+            //http://www.devx.com/tips/Tip/21168
+            XmlDocument XmlDoc = new XmlDocument();
+
+            XmlNode rootNode = XmlDoc.CreateElement("message");
+            XmlDoc.AppendChild(rootNode);
+
+            XmlNode parendNode = XmlDoc.CreateElement("from");
+            parendNode.InnerText = mes.from;
+            rootNode.AppendChild(parendNode);
+
+            parendNode = XmlDoc.CreateElement("to");
+            parendNode.InnerText = mes.to;
+            rootNode.AppendChild(parendNode);
+
+            parendNode = XmlDoc.CreateElement("id");
+            parendNode.InnerText = mes.Id;
+            rootNode.AppendChild(parendNode);
+
+            parendNode = XmlDoc.CreateElement("body");
+            parendNode.InnerText = mes.body;
+            rootNode.AppendChild(parendNode);
+
+            parendNode = XmlDoc.CreateElement("komenda");
+            parendNode.InnerText = mes.komenda.ToString();
+            rootNode.AppendChild(parendNode);
+
+            byte[] bajty = Encoding.Default.GetBytes(XmlDoc.OuterXml);
+            return bajty;
+        }
+    }
+}
--- Messages/Messages.cs
+++ Messages/Messages.cs
@@ -11,10 +11,51 @@ namespace Messages
     {
         private string id;
         public String Id { get; }
-        public Messages()
+        public string to;
+        public string from;
+        public string body;
+        public Komendy komenda;
+        public Messages(string from, Komendy komenda)
         {
             id = Guid.NewGuid().ToString();
+            this.komenda = komenda;
         }
-
+        public Messages(string from, string body, Komendy komenda)
+        {
+            id = Guid.NewGuid().ToString();
+            this.from = from;
+            this.body = body;
+            this.komenda = komenda;
+        }
+        public Messages (string from, string to, string body, Komendy komenda)
+        {
+            id = Guid.NewGuid().ToString();
+            this.komenda = komenda;
+            this.from = from;
+            this.to = to;
+            this.body = body;
+        }
+        public Messages(string id)
+        {
+            this.id = id;
+        }
+        public void stringToKomenda(string komendaString)
+        {
+            switch(komendaString.ToUpper())
+            {
+                case "LOGIN":
+                    komenda = Komendy.Login;
+                    break;
+                case "LOGOUT":
+                    komenda = Komendy.Logout;
+                    break;
+                case "TEXTMESSAGE":
+                    komenda = Komendy.TextMessage;
+                    break;
+                default:
+                    komenda = Komendy.Help;
+                    break;
+            }
+        }
     }
 }
